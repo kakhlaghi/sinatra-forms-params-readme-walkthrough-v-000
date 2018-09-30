281517edea52bf0a72013d8202c2af6fbd1c5a9c
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-30 23:04:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,10 +10,7 @@ class App < Sinatra::Base
   <form method="POST" action="/food">
 
   post '/food' do
-    params = {
-      :name => "Sam",
-      :favorite_food => "Green Eggs and Ham"
-    }
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
   end
 
 end
